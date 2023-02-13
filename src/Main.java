c9e922d8b1a564739e036300e8aa58f543ab02bf
Edward-Kazan-Skypro <zvezda_torgi@mail.ru>
2023-02-13 13:38:09 +0300

    первый коммит от feature-1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Random numbers");
+        for (int i = 0; i < 20; i++) {
+            System.out.print((int) (Math.random() * 100) + " ");
+        }
+        System.out.println("end");
+
     }
 }
